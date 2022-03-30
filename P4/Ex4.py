da813a7609aa23a2c8da820b06381f17ddf2c4e1
zai_beluna <z.benitez.2021@alumnos.urjc.es>
2022-03-30 18:19:21 +0200

    30-03-2022

---
+++ P4/Ex4.py
@@ -0,0 +1,59 @@
+import socket
+import termcolor
+from pathlib import Path
+
+IP = "127.0.0.1"
+PORT = 8080
+
+
+def process_client(s):
+    req_raw = s.recv(2000)
+    req = req_raw.decode()
+
+    print("Message FROM CLIENT: ")
+
+    lines = req.split('\n')
+    req_line = lines[0]
+    slices = req_line.split(" ")
+    method = slices[0]  # "GET"
+    path = slices[1]  # "/directory/other/file.html"
+    version = slices[2]  # "HTTP/1.0"
+    print("Request line: ", end="")
+    termcolor.cprint(req_line, "green")
+
+    if path == "/info/G":
+        body = Path("index.html").read_text()
+        status_line = "HTTP/1.1 200 OK\n"
+        headers = "Content-Type: text/html\n"
+        headers += f"Content-Length: {len(body)}\n"
+        response_msg = status_line + headers + "\n" + body
+        cs.send(response_msg.encode())
+
+    if path == "/info/T":
+        body = Path("index.html").read_text()
+        status_line = "HTTP/1.1 200 OK\n"
+        headers = "Content-Type: text/html\n"
+        headers += f"Content-Length: {len(body)}\n"
+        response_msg = status_line + headers + "\n" + body
+        cs.send(response_msg.encode())
+
+ls = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ls.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+ls.bind((IP, PORT))
+ls.listen()
+
+print("SEQ Server configured!")
+
+while True:
+    print("Waiting for clients....")
+    try:
+        (cs, client_ip_port) = ls.accept()
+    except KeyboardInterrupt:
+        print("Server Stopped!")
+        ls.close()
+        exit()
+    else:
+        process_client(cs)
+
+        cs.close()
---
+++ P5/Bases2-webserver.py
@@ -0,0 +1,101 @@
+import http.server
+import socketserver
+import termcolor
+from pathlib import Path
+
+IP = "localhost"
+PORT = 8080
+
+socketserver.TCPServer.allow_reuse_address = True
+
+class TestHandler(http.server.BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        """This method is called whenever the client invokes the GET method
+        in the HTTP protocol request"""
+
+        termcolor.cprint(self.requestline, 'green')
+
+        contents = "I am the happy server! :-)"
+
+        self.send_response(200)
+
+        self.send_header('Content-Type', 'text/plain')
+        self.send_header('Content-Length', len(contents.encode()))
+
+        self.end_headers()
+
+        self.wfile.write(contents.encode())
+
+        return
+
+    def process_client(s):
+        req_raw = s.recv(2000)
+        req = req_raw.decode()
+
+        lines = req.split('\n')
+        req_line = lines[0]
+        slices = req_line.split(" ")
+        path = slices[1]
+        print("Request line: ", end="")
+        termcolor.cprint(req_line, "green")
+
+        if path == "/info/A":
+            body = Path("index.html").read_text()
+            status_line = "HTTP/1.1 200 OK\n"
+            headers = "Content-Type: text/html\n"
+            headers += f"Content-Length: {len(body)}\n"
+            response_msg = status_line + headers + "\n" + body
+            cs.send(response_msg.encode())
+
+        elif path == "/info/C":
+            body = Path("index.html").read_text()
+            status_line = "HTTP/1.1 200 OK\n"
+            headers = "Content-Type: text/html\n"
+            headers += f"Content-Length: {len(body)}\n"
+            response_msg = status_line + headers + "\n" + body
+            cs.send(response_msg.encode())
+
+        elif path == "/info/G":
+            body = Path("index.html").read_text()
+            status_line = "HTTP/1.1 200 OK\n"
+            headers = "Content-Type: text/html\n"
+            headers += f"Content-Length: {len(body)}\n"
+            response_msg = status_line + headers + "\n" + body
+            cs.send(response_msg.encode())
+
+        elif path == "/info/T":
+            body = Path("index.html").read_text()
+            status_line = "HTTP/1.1 200 OK\n"
+            headers = "Content-Type: text/html\n"
+            headers += f"Content-Length: {len(body)}\n"
+            response_msg = status_line + headers + "\n" + body
+            cs.send(response_msg.encode())
+
+        elif path == "/Error.html":
+            body = Path("Error.html").read_text()
+            status_line = "HTTP/1.1 200 OK\n"
+            headers = "Content-Type: text/html\n"
+            headers += f"Content-Length: {len(body)}\n"
+            response_msg = status_line + headers + "\n" + body
+            cs.send(response_msg.encode())
+
+        elif path == "/index.html":
+            body = Path("index.html").read_text()
+            status_line = "HTTP/1.1 200 OK\n"
+            headers = "Content-Type: text/html\n"
+            headers += f"Content-Length: {len(body)}\n"
+            response_msg = status_line + headers + "\n" + body
+            cs.send(response_msg.encode())
+
+Handler = TestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("Serving at PORT", PORT)
+
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print("")
+        print("Stopped by the user")
+        httpd.server_close()
